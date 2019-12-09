ace7f979f1ebda3a371470fef717e79a5c714e69
Cosmin Butnaru <cosminbutnaru18@gmail.com>
2019-12-09 13:27:51 +0200

    Add project files.

---
+++ PaginationHelper/PaginationHelper.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PaginationHelper
+{
+    public class PaginationHelper<T>
+    {
+        // TODO: Complete this class
+
+        /// <summary>
+        /// Constructor, takes in a list of items and the number of items that fit within a single page
+        /// </summary>
+        /// <param name="collection">A list of items</param>
+        /// <param name="itemsPerPage">The number of items that fit within a single page</param>
+        ///
+        public IList<T> Collection { get; set; }
+        public int ItemsPerPage { get; set; }
+
+        public PaginationHelper(IList<T> collection, int itemsPerPage)
+        {
+            Collection = collection;
+            ItemsPerPage = itemsPerPage;
+        }
+
+        /// <summary>
+        /// The number of items within the collection
+        /// </summary>
+        public int ItemCount
+        {
+            get
+            {
+                return Collection.Count();
+            }
+        }
+
+        /// <summary>
+        /// The number of pages
+        /// </summary>
+        public int PageCount
+        {
+            get
+            {
+                return (Collection.Count() / ItemsPerPage) + 1;
+            }
+        }
+
+        /// <summary>
+        /// Returns the number of items in the page at the given page index 
+        /// </summary>
+        /// <param name="pageIndex">The zero-based page index to get the number of items for</param>
+        /// <returns>The number of items on the specified page or -1 for pageIndex values that are out of range</returns>
+        public int PageItemCount(int pageIndex)
+        {
+            if (pageIndex >= PageCount || pageIndex < 0)
+                return -1;
+            else
+                if (pageIndex < PageCount - 1)
+                return ItemsPerPage;
+            else
+                return Collection.Count % ItemsPerPage;
+        }
+
+        /// <summary>
+        /// Returns the page index of the page containing the item at the given item index.
+        /// </summary>
+        /// <param name="itemIndex">The zero-based index of the item to get the pageIndex for</param>
+        /// <returns>The zero-based page index of the page containing the item at the given item index or -1 if the item index is out of range</returns>
+        public int PageIndex(int itemIndex)
+        {
+            if (itemIndex < 0 || itemIndex >= Collection.Count)
+                return -1;
+            else
+                return itemIndex / ItemsPerPage;
+        }
+    }
+}
---
+++ UnitTest.PaginationHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest.PaginationHelper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest.PaginationHelper")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("01e13207-94ba-45bb-a3e4-6e84ea4d96bc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
